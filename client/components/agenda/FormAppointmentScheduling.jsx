5325f39a87e2ec1fd74f94e7780c972f2a49f672
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-10 23:34:20 -0300

    add logic of forms component

--- client/components/agenda/FormAppointmentScheduling.jsx
+++ client/components/agenda/FormAppointmentScheduling.jsx
@@ -1,45 +1,30 @@
-import { useState } from "react";
+"use client"
 
+import {useAppointmentSchedulingContext} from "@/contexts/AppointmentSchedulingProvider";
+import { useEffect } from "react";
 const FormAgenda = () => {
 
-  const [agendaTemplate, setAgendaTemplate] = useState({
-    name: '',
-    lastName: '',
-    celPhone: '',
-    email: ''
-  })
-  const [errors, setErrors] = useState("")
+  const {formDataAppointmentScheduling, handleChange, errors} = useAppointmentSchedulingContext()
 
-  const handleChange = (e) => {
-    setAgendaTemplate({
-      ...agendaTemplate,
-      [e.target.name]: e.target.value,
-    })
-    setErrors(
-      validationErrorsLogin({
-        ...agendaTemplate,
-        [e.target.name]: e.target.value,
-      }),
-    )
-  }
+  useEffect(() => {console.log(formDataAppointmentScheduling);}, [formDataAppointmentScheduling])
 
   return (
-    <div className="flex ">
-      <div>
+    <div className="w-full flex items-center justify-center flex-wrap gap-x-24 gap-y-8">
+      <div className="w-72">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-        <input type="text" onChange={handleChange} id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" />
+        <input type="text" value={formDataAppointmentScheduling.name} onChange={(e)=>handleChange(e)} name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="John" />
       </div>
-      <div>
+      <div className="w-72">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido</label>
-        <input type="text" onChange={handleChange} id="lastName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" />
+        <input type="text" value={formDataAppointmentScheduling.lastName} onChange={(e)=>handleChange(e)} name="lastName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Doe" />
       </div>
-      <div>
+      <div className="w-72">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefono</label>
-        <input type="text" onChange={handleChange} id="celPhone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="543865559022" />
+        <input type="text" value={formDataAppointmentScheduling.celPhone} onChange={(e)=>handleChange(e)} name="celPhone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="543865559022" />
       </div>
-      <div>
+      <div className="w-72">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-        <input type="text" onChange={handleChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@dominio.com" />
+        <input type="text" value={formDataAppointmentScheduling.email} onChange={(e)=>handleChange(e)} name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="email@dominio.com" />
       </div>
     </div>
   )
