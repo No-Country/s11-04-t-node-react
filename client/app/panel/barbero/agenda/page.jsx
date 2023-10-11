5de2447144ee904ff396a6bbcaa326e047c8538c
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-10 23:33:37 -0300

    create and add appointmentscheduling provider

--- client/app/panel/barbero/agenda/page.jsx
+++ client/app/panel/barbero/agenda/page.jsx
@@ -1,21 +1,25 @@
 "use client"
+
+import { AppointmentSchedulingProvider } from '@/contexts/AppointmentSchedulingProvider.jsx'
+
+import CalendarAppointmentScheduling from "@/components/agenda/CalendarAppointmentScheduling";
 import FormAgenda from "@/components/agenda/FormAppointmentScheduling";
-import { Calendar } from "@/components/ui/calendar"
-import { useState, useEffect } from "react";
+import SelectService from '@/components/agenda/SelectService';
 
 export default function AppointmentSchedulingPage() {
-  const [date, setDate] = useState(new Date())
-  useEffect(() => { console.log(date); }, [date])
+  
   return (
-    <div className="w-full flex flex-col items-center justify-center min-h-screen bg-[#D9D9D9]">
-      <h3>Crear turno</h3>
-      <FormAgenda/>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-      />
-    </div>
+    <AppointmentSchedulingProvider>
+      <div className="p-24 mx-auto w-full flex flex-col items-center min-h-screen bg-[#D9D9D9] gap-y-12">
+        <h3 className='self-start text-3xl font-black'>Crear turno</h3>
+        <div className='flex flex-col w-3/4 gap-y-12'>
+          <FormAgenda />
+          <div className='flex items-start w-full justify-between'>
+            <CalendarAppointmentScheduling/>
+            <SelectService/>
+          </div>
+        </div>
+      </div>
+    </AppointmentSchedulingProvider>
   );
 }
