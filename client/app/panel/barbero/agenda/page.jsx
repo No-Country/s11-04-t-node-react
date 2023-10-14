f6c8043772ac7a7e02b7362bd6dece9597b8bdbd
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-14 11:02:23 -0300

    grid components

--- client/app/panel/barbero/agenda/page.jsx
+++ client/app/panel/barbero/agenda/page.jsx
@@ -3,22 +3,20 @@
 import { AppointmentSchedulingProvider } from '@/contexts/AppointmentSchedulingProvider.jsx'
 
 import CalendarAppointmentScheduling from "@/components/agenda/CalendarAppointmentScheduling";
-import FormAgenda from "@/components/agenda/FormAppointmentScheduling";
-import SelectService from '@/components/agenda/SelectService';
+import Hours from '@/components/agenda/Hours';
+import PopUpCreateCite from '@/components/agenda/PopUpCreateCite';
 
 export default function AppointmentSchedulingPage() {
-  
+
   return (
     <AppointmentSchedulingProvider>
-      <div className="p-24 mx-auto w-full flex flex-col items-center min-h-screen bg-[#D9D9D9] gap-y-12">
+      <div className="p-12 mx-auto w-full flex flex-col items-center min-h-screen bg-[#D9D9D9] gap-y-12">
         <h3 className='self-start text-3xl font-black'>Crear turno</h3>
-        <div className='flex flex-col w-3/4 gap-y-12'>
-          <FormAgenda />
-          <div className='flex items-start w-full justify-between'>
-            <CalendarAppointmentScheduling/>
-            <SelectService/>
-          </div>
+        <div className='w-full flex justify-between'>
+          <Hours/>
+          <CalendarAppointmentScheduling/>
         </div>
+        <PopUpCreateCite/>
       </div>
     </AppointmentSchedulingProvider>
   );
