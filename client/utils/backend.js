a7945f214ebec650296f5fa3c8da98367fbb875f
emilio rivas <emiliorivasruiz@gmail.com>
2023-10-16 17:39:16 -0500

    agrego dominio del back

--- client/utils/backend.js
+++ client/utils/backend.js
@@ -4,8 +4,6 @@ export const backend = axios.create({
   baseURL: "https://barberbuddy.fly.dev/api/v1/",
 });
 
-
-
 export const getAuthorization = (token) => {
   const getToken = token || sessionStorage.getItem("token");
   const Headers = {
