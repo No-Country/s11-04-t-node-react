fa65827745bf4520940660ce1854d797836c74e9
juancgalueweb <juancgalue@icloud.com>
2023-10-20 00:20:24 -0300

    Agrega middlewares necesarios

--- server/src/routes/appointment.route.ts
+++ server/src/routes/appointment.route.ts
@@ -3,8 +3,23 @@ import {
   deleteAppointment,
   modifyAppointment
 } from '../controllers/appointment.controller'
+import { auth } from '../middlewares/auth.middleware'
+import { extractToken } from '../middlewares/extractToken.middleware'
+import { isBarberOrAdmin } from '../middlewares/role.middlewares'
 
 export const appoimentRouter = Router()
 
-appoimentRouter.delete('/delete/:id', deleteAppointment)
-appoimentRouter.put('/modify/:id', modifyAppointment)
+appoimentRouter.delete(
+  '/delete/:id',
+  extractToken,
+  auth,
+  isBarberOrAdmin,
+  deleteAppointment
+)
+appoimentRouter.put(
+  '/modify/:id',
+  extractToken,
+  auth,
+  isBarberOrAdmin,
+  modifyAppointment
+)
