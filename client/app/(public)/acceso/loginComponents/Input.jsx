dc25e92c97b5d667bcb272520b1335149b6e4c69
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-10-24 12:47:40 -0400

    cambios en estilado de OTP input box, pagina Home a login

--- client/app/(public)/acceso/loginComponents/Input.jsx
+++ client/app/(public)/acceso/loginComponents/Input.jsx
@@ -1,21 +1,34 @@
-const Input = ({ type, name, event, onBlur, value, label, style, placeholder }) => {
-  const className =
-    "outline-none pl-1 border-b-2 border-gray-200 rounded-xl " + style;
-  return (
-    <div className="grid w-full">
-      <label htmlFor={name} className="capitalize text-2xl tracking-widest py-1 text-white font-medium">{label}</label>
-      <input
-        required
-        className={className}
-        type={type}
-        name={name}
-        id={name}
-        onChange={event}
-        onBlur={onBlur}
-        value={value}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
-export default Input;
+const Input = ({
+	type,
+	name,
+	event,
+	onBlur,
+	value,
+	label,
+	style,
+	placeholder,
+}) => {
+	const className = `outline-none pl-1 border rounded-xl border-slate-300 ${style}`
+	return (
+		<div className="grid w-full">
+			<label
+				htmlFor={name}
+				className="text-xl tracking-widest pb-2 text-white font-normal"
+			>
+				{label}
+			</label>
+			<input
+				required
+				className={className}
+				type={type}
+				name={name}
+				id={name}
+				onChange={event}
+				onBlur={onBlur}
+				value={value}
+				placeholder={placeholder}
+			/>
+		</div>
+	)
+}
+export default Input
