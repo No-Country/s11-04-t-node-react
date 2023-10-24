1f0aac795ebee1079ac34def1ca0582404dc6410
emanuelpps <emanuelpages.ps@gmail.com>
2023-10-24 15:27:27 -0300

    modal overflow correction

--- client/app/panel/admin/clientes/components/ClientModify.jsx
+++ client/app/panel/admin/clientes/components/ClientModify.jsx
@@ -8,7 +8,7 @@ function ClientModify({
   return modalModifyClient ? (
     <div
       id="modal-clients-container"
-      className="bg-[#292D33] text-white border rounded-lg p-5 absolute top-[12vh] sm:top-1/2 h-[75vh] sm:h-fit overflow-hidden overflow-y-scroll left-1/2 -translate-x-1/2 sm:-translate-x-1/4 sm:-translate-y-1/2"
+      className="bg-[#292D33] text-white border rounded-lg p-5 absolute top-[12vh] sm:top-1/2 h-[75vh] sm:h-fit overflow-hidden left-1/2 -translate-x-1/2 sm:-translate-x-1/4 sm:-translate-y-1/2"
     >
       <div className="flex justify-end">
         <button onClick={() => setModalModifyClient(false)}>X</button>
