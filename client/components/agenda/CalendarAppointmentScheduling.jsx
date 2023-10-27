f9da4f3e7182981791f3465ef2baa122ecadcddb
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-27 14:38:02 -0300

    refixed

--- client/components/agenda/CalendarAppointmentScheduling.jsx
+++ client/components/agenda/CalendarAppointmentScheduling.jsx
@@ -5,18 +5,10 @@ import { useAppointmentSchedulingContext } from '@/contexts/AppointmentSchedulin
 
 import { Calendar } from '@/components/ui/calendar'
 import axios from 'axios'
+import { formatDate } from './utils'
 
 const CalendarAppointmentScheduling = () => {
   const { formDataAppointmentScheduling, setFormDataAppointmentScheduling, date, setDate, setAppointments } = useAppointmentSchedulingContext()
-
-  function formatDate(inputDate) {
-    const date = new Date(inputDate);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-
-    return `${day}-${month}-${year}`;
-  }
 
   const getAppointmentsForDate = async (actualDate) => {
     try {
@@ -57,74 +49,3 @@ const CalendarAppointmentScheduling = () => {
   )
 }
 export default CalendarAppointmentScheduling
-
-
-// barberId
-// :
-// email
-// :
-// "fabiuuu8@gmail.com"
-// fullName
-// :
-// "Fabian Carabajal"
-// _id
-// :
-// "6526dd8ce90fa3ae5d1e176f"
-// [[Prototype]]
-// :
-// Object
-// clientId
-// :
-// email
-// :
-// "emaaa@gmail.com"
-// fullName
-// :
-// "emma"
-// _id
-// :
-// "6536d474aac66ade5d43f0ab"
-// [[Prototype]]
-// :
-// Object
-// date
-// :
-// "25-10-2023"
-// endTime
-// :
-// "08:30"
-// services
-// :
-// Array(1)
-// 0
-// :
-// name
-// :
-// "barba"
-// price
-// :
-// 5000
-// _id
-// :
-// "652e199feeb638c5bb91edf5"
-// [[Prototype]]
-// :
-// Object
-// length
-// :
-// 1
-// [[Prototype]]
-// :
-// Array(0)
-// startTime
-// :
-// "08:00"
-// status
-// :
-// "pending"
-// totalPrice
-// :
-// 5000
-// _id
-// :
-// "653910fb88c24cd20846a087"
