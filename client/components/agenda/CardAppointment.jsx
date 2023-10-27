9d10f95a283e9df48c7ac92f3cf9a1dae14eacb7
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-27 14:38:36 -0300

    add logic in card

--- client/components/agenda/CardAppointment.jsx
+++ client/components/agenda/CardAppointment.jsx
@@ -5,12 +5,29 @@ import { AiFillClockCircle, AiFillCheckCircle } from 'react-icons/ai'
 import { MdCancel } from 'react-icons/md'
 
 
-const CardAppointment = ({ clientId, totalPrice, status, services }) => {
-  const { hiddenEditAppointment, setHiddenEditAppointment } = useAppointmentSchedulingContext()
+const CardAppointment = ({ clientId, totalPrice, status, services, startTime, endTime, date, barberId, appointmentId }) => {
 
-  
+  const { setFormDataAppointmentScheduling, hiddenPopUp, setHiddenPopUp, flagEdit, setFlagEdit, setAppointmentEditId, setClient } = useAppointmentSchedulingContext()
+
+
   return (
-    <button onClick={() => setHiddenEditAppointment(!hiddenEditAppointment)} className="w-4/5 flex flex-col gap-y-2 px-4 py-2 bg-yellow-200 rounded-lg hover:scale-105 hover:cursor-pointer">
+    <button onClick={() => {
+      setClient({
+        clientId: clientId._id, 
+        fullName: clientId.fullName
+      })
+      setFormDataAppointmentScheduling({
+        clientId: clientId._id, 
+        barberId: barberId._id,
+        services: services.map(service => service._id), 
+        startTime, 
+        endTime, 
+        date 
+      })
+      setAppointmentEditId(appointmentId)
+      setHiddenPopUp(!hiddenPopUp)
+      setFlagEdit(true)
+    }} className="w-4/5 flex flex-col gap-y-2 px-4 py-2 bg-yellow-200 rounded-lg hover:scale-105 hover:cursor-pointer">
       <div className="w-full flex items-center justify-between">
         <div className='flex items-center gap-x-3'>
           <span className="text-black font-semibold uppercase">{clientId?.fullName}</span>
@@ -18,11 +35,11 @@ const CardAppointment = ({ clientId, totalPrice, status, services }) => {
         </div>
         <span className="rounded-md p-1 text-xl text-black">
           {
-            status == 'pending' ? 
-            <AiFillClockCircle/> : 
-            status == 'cancelled' ?
-            <MdCancel/> : 
-            <AiFillCheckCircle/> 
+            status == 'pending' ?
+              <AiFillClockCircle /> :
+              status == 'cancelled' ?
+                <MdCancel /> :
+                <AiFillCheckCircle />
           }
         </span>
       </div>
