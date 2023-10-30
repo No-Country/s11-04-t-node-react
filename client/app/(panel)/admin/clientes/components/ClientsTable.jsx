8a1fd5573d356e33453082b608cce7a3543fb10a
emanuelpps <emanuelpages.ps@gmail.com>
2023-10-30 17:04:08 -0300

    correcciones en icons del client table

--- client/app/(panel)/admin/clientes/components/ClientsTable.jsx
+++ client/app/(panel)/admin/clientes/components/ClientsTable.jsx
@@ -6,6 +6,9 @@ import agendaIcon from "@/public/images/agenda-icon.svg";
 import ClientsModal from "./ClientsModal";
 import Link from "next/link";
 import { Notification } from "./Notification";
+import { TbEdit } from "react-icons/tb";
+import { TbHistory } from "react-icons/tb";
+import { TbCalendarTime } from "react-icons/tb";
 
 export default function ClientsTable({
   setShowHistory,
@@ -61,29 +64,26 @@ export default function ClientsTable({
                   </td>
                   <td className="text-center text-xs sm:text-sm pt-3 pb-3">
                     <button
+                      className="text-blue-700 hover:text-blue-900 transition text-3xl"
                       onClick={() => {
                         setClientAppointmentId(client._id);
                         showModalHandler();
                         setClientId(client);
                       }}
                     >
-                      <Image
-                        src={historiaLogo}
+                      <TbHistory
                         width={30}
                         height={30}
                         alt="Historial de Cliente"
-                      ></Image>
+                      />
                     </button>
                   </td>
                   <td className="text-center text-xs sm:text-sm flex justify-center pt-3 pb-3">
-                    <button onClick={() => showClientHandler(client._id)}>
-                      <Image
-                        className=""
-                        src={editLogo}
-                        width={30}
-                        height={30}
-                        alt="Editar Cliente"
-                      ></Image>
+                    <button
+                      className="text-amber-500 hover:text-amber-700 transition text-3xl"
+                      onClick={() => showClientHandler(client._id)}
+                    >
+                      <TbEdit src={editLogo} width={30} height={30} />
                     </button>
                   </td>
                   <td className="text-center text-xs  sm:text-sm pt-3 pb-3">
@@ -91,14 +91,12 @@ export default function ClientsTable({
                       key={client._id}
                       href={`agenda?id=${client._id}?name=${client.fullName}`}
                     >
-                      <button>
-                        <Image
-                          className=""
-                          src={agendaIcon}
+                      <button className="text-green-700 hover:text-green-900 transition text-3xl">
+                        <TbCalendarTime
                           width={30}
                           height={30}
                           alt="Agendar Turno"
-                        ></Image>
+                        />
                       </button>
                     </Link>
                   </td>
